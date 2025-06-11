d2d66ad6b4d2697d792990b5fa389f14dd7780c8
ausdotsn50 <ausdotsn50@gmail.com>
2025-06-12 01:18:20 +0800

    button and anchors fixed

---
+++ contact.js
@@ -0,0 +1,27 @@
+// Country droplist
+var selectedCountries = ['Greece', 'Argentina', 'Japan', 'United States', 'Philippines'];
+
+// select <ul> with id #countryList
+const countryListContainer = document.querySelector("#countryList");
+
+for(let i = 0; i < selectedCountries.length; i++) {
+    // create new list
+    const newList = document.createElement("li");
+    newList.innerHTML = ('<button class="dropdown-item">' + selectedCountries[i] + '</button>'); // for escaping purposes
+
+    newList.setAttribute('id', 'elem' + i);
+
+    // append new list to 
+    countryListContainer.appendChild(newList);
+
+    document.querySelector('#elem' + i).addEventListener('click', function() {
+        document.querySelector('#country').textContent = selectedCountries[i];
+    });
+}
+
+// when formSubmit id is clicked, alert + ...
+document.querySelector('#formSubmit').addEventListener('click', function() {
+    alert('Form successfully submitted')
+});
+
+
--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-// select <ul> with id #countryList
-const countryList = document.querySelector("#countryList");
-
-// create new list
-const newList = document.createElement("li");
-newList.innerHTML = ('<a class="dropdown-item" href="#">Default List Item</a>'); // for escaping purposes
-
-// append new list to 
-countryList.appendChild(newList);
+// when active class is clicked, scroll on top
+// navigation
+document.querySelector('.active').addEventListener('click', function() {
+    window.scrollTo(0, 0);
+});
